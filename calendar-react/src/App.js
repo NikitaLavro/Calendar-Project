8113a0d44441320b4499c078c60765bf0965f0dc
Nikita <lavro.nikita123@gmail.com>
2023-01-27 12:19:06 -0500

    month changing logic

--- calendar-react/src/App.js
+++ calendar-react/src/App.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import "./index.css";
+import { useState, useContext, useEffect } from "react";
 
 //Helpers
 import { getDaysInMonth } from "./helpers/helpers";
@@ -9,10 +8,19 @@ import CalendarHeader from "./components/CalendarHeader";
 import Month from "./components/Month";
 import Sidebar from "./components/Sidebar";
 
+//Context
+import AppContext from "./context/AppContext";
+
 console.table(getDaysInMonth());
 
 function App() {
   const [currentMonth, setCurrentMonth] = useState(getDaysInMonth());
+  const { monthIndex } = useContext(AppContext);
+
+  useEffect(() => {
+    setCurrentMonth(getDaysInMonth(monthIndex));
+  }, [monthIndex]);
+
   return (
     <>
       <div className="h-screen flex flex-col">
--- calendar-react/src/components/CalendarHeader.jsx
+++ calendar-react/src/components/CalendarHeader.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import dayjs from "dayjs";
 
 //MUI Icons
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
+import AppContext from "../context/AppContext";
+
 const CalendarHeader = () => {
+  const { monthIndex, setMonthIndex } = useContext(AppContext);
+
+  const handlePrevMonth = () => {
+    setMonthIndex(monthIndex - 1);
+  };
+
+  const handleNextMonth = () => {
+    setMonthIndex(monthIndex + 1);
+  };
+
   return (
     <header className="px-4 py-2 flex items-center">
       <h1 className="mr-10 text-xl text-gray-500 font-bold">Calendar App</h1>
       <button className="border rounded py-2 px-4 mr-5">Today</button>
-      <button>
+      <button onClick={handlePrevMonth}>
         <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
           {<ChevronLeftIcon />}
         </span>
       </button>
-      <button>
+      <h2>{dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}</h2>
+
+      <button onClick={handleNextMonth}>
         <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
           {<ChevronRightIcon />}
         </span>
