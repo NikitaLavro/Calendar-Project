d4c9741e2d25cfe58b43e88e03b687a0a5ca377c
Nikita <lavro.nikita123@gmail.com>
2023-01-27 11:31:20 -0500

    highlight current day functionality

--- calendar-react/src/components/Day.jsx
+++ calendar-react/src/components/Day.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import dayjs from "dayjs";
 
 const Day = ({ day, rowIndex }) => {
+  const currentDayClass = () => {
+    return day.format("DD--MM--YY") === dayjs().format("DD--MM--YY")
+      ? "bg-emerald-200"
+      : "";
+  };
   return (
-    <div className="border border-gray-200 flex flex-col">
+    <div
+      className={`border border-gray-200 flex flex-col ${currentDayClass()}`}
+    >
       <header className="flex flex-col items-center">
         {rowIndex === 0 && (
           <p className="text-small mt-1">{day.format("ddd").toUpperCase()}</p>
